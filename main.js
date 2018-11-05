7e601e65f9d0f9f95c407b91da487b3b8b1f734e
KennyCaswell <44781441+KennyCaswell@users.noreply.github.com>
2018-11-05 16:08:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-var ("oysters") = 0;
-function oysterClick(1){oysters = oysters + 1 };
+var ("oyster") = 0;
+function oysterClick(1){oysters = oyster + 1 };
+document.getElementById("oysters").innerHTML = oyster;
 
